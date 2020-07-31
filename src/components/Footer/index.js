264d8b55d23ecc54f18a7c1584784e7942c03a21
bruno paulo da silva <brunopaulodasilva33@gmail.com>
2020-07-31 00:01:55 -0300

    arquivo

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,10 +8,10 @@ function Footer() {
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
       <p>
-        Orgulhosamente criado durante a
+        Sejam bem vindos Galera
         {' '}
         <a href="https://www.alura.com.br/">
-          Imersão React da Alura
+          Imersão é pra já
         </a>
       </p>
     </FooterBase>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
 
+import {BrowserRouter, Switch,Route} from 'react-router-dom';//essa importaçao da agilidade
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/cadastrocategoria';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Pagina404 = () => (<div>Página 404</div>)
+
+ReactDOM.render( //essa importaçao da agilidade ,e a função exact deixa tela em branco, 
+                //e route sem path error404 quando nao tiver rota especifica certa
+  <BrowserRouter>
+     <Switch>
+        <Route path="/" component={Home} exact /> 
+        <Route path="/cadastro/video" component= {CadastroVideo} />
+        <Route path="/cadastro/categoria" component= {CadastroCategoria} />
+        <Route component={Pagina404}/>  
+          
+                                                 
+     </Switch>
+         </BrowserRouter>,
   document.getElementById('root')
 );
+
+
 
 
